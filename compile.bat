e6e915a80de50bfe4c5ddcabb068c1158b993531
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2025-04-03 15:48:07 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -2,6 +2,8 @@ cd src
 clang -target x86_64-windows -DUTILS_EXPORTS -shared utils.c -o standard.dll -Wl,/DLL,/IMPLIB:standard.lib -I.
 del standard.exp
 cd ..
+if not exist build mkdir build
+if not exist include mkdir include
 move src\standard.dll build\standard.dll
 move src\standard.lib build\standard.lib
 copy src\utils.h include\
